4d0856395492045f891dd749d97a57f24ad0b7f8
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-05-12 15:05:53 +0200

    Fixed the bug where db inserts a new entity instead of updating an existing one

--- src/main/java/com/productdock/library/inventory/book/InventoryRecordRepository.java
+++ src/main/java/com/productdock/library/inventory/book/InventoryRecordRepository.java
@@ -3,7 +3,10 @@ package com.productdock.library.inventory.book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InventoryRecordRepository extends MongoRepository<InventoryRecordEntity, String> {
 
+    Optional<InventoryRecordEntity> findByBookId(String bookId);
 }
--- src/main/java/com/productdock/library/inventory/book/InventoryRecordService.java
+++ src/main/java/com/productdock/library/inventory/book/InventoryRecordService.java
@@ -1,24 +1,25 @@
 package com.productdock.library.inventory.book;
 
 import com.productdock.library.inventory.domain.Inventory;
+import com.productdock.library.inventory.domain.RentalRecord;
 import com.productdock.library.inventory.exception.InventoryException;
 import com.productdock.library.inventory.producer.Publisher;
-import com.productdock.library.inventory.domain.RentalRecord;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public record InventoryRecordService(InventoryRecordRepository inventoryRecordRepository, Publisher publisher, InventoryRecordMapper inventoryRecordMapper) {
+public record InventoryRecordService(InventoryRecordRepository inventoryRecordRepository, Publisher publisher,
+                                     InventoryRecordMapper inventoryRecordMapper) {
 
     public void updateBookState(RentalRecord rentalRecord) throws Exception {
         var book = getInventoryFrom(rentalRecord.getBookId());
         book.updateStateWith(rentalRecord);
-        inventoryRecordRepository.save(inventoryRecordMapper.toEntity(book));
+        saveInventoryRecord(book);
         publisher.sendMessage(new BookAvailabilityMessage(book.getBookId(), book.getAvailableBooksCount()));
     }
 
     private Inventory getInventoryFrom(String bookId) {
-        var optionalBook = inventoryRecordRepository.findById(bookId);
+        var optionalBook = inventoryRecordRepository.findByBookId(bookId);
         if (optionalBook.isEmpty()) {
             throw new InventoryException("Book does not exist in inventory!");
         }
@@ -26,8 +27,17 @@ public record InventoryRecordService(InventoryRecordRepository inventoryRecordRe
         return inventoryRecordMapper.toDomain(bookEntity);
     }
 
-    public int getAvailableBooksCount(String bookId){
+    public int getAvailableBooksCount(String bookId) {
         var book = getInventoryFrom(bookId);
         return book.getAvailableBooksCount();
+    }
+
+    private void saveInventoryRecord(Inventory book) {
+        var previousRecordEntity = inventoryRecordRepository.findByBookId(book.getBookId());
+        var newRecordEntity = inventoryRecordMapper.toEntity(book);
+        if (previousRecordEntity.isPresent()) {
+            newRecordEntity.setId(previousRecordEntity.get().getId());
+        }
+        inventoryRecordRepository.save(newRecordEntity);
     }
 }
