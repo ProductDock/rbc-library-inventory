4893946ebcaf2c3e6b46ed85b0805868a8b5837c
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-05-13 14:07:04 +0200

    Refactoring for consumer test

--- src/main/java/com/productdock/library/inventory/book/InventoryRecordService.java
+++ src/main/java/com/productdock/library/inventory/book/InventoryRecordService.java
@@ -22,6 +22,8 @@ public record InventoryRecordService(InventoryRecordRepository inventoryRecordRe
 
     private Inventory getInventoryFrom(String bookId) {
         var optionalBook = inventoryRecordRepository.findByBookId(bookId);
+        System.out.println(inventoryRecordRepository.findByBookId(bookId));
+        System.out.println(optionalBook);
         if (optionalBook.isEmpty()) {
             throw new InventoryException("Book does not exist in inventory!");
         }
