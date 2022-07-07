d3d1f0da724cb64d3b157f430edaf603f6e5702a
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-07-07 13:28:24 +0200

    Refactoring

--- src/main/java/com/productdock/library/inventory/domain/RentalRecordMapper.java
+++ src/main/java/com/productdock/library/inventory/domain/RentalRecordMapper.java
@@ -1,11 +1,9 @@
 package com.productdock.library.inventory.domain;
 
 import com.productdock.library.inventory.adapter.in.kafka.messages.RentalRecordMessage;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+@Component
 public class RentalRecordMapper {
 
     public RentalRecord toDomain(RentalRecordMessage source) {
