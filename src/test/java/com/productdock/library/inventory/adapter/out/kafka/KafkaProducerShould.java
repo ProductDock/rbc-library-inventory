0467325e8d88e580af4c9578f621ff5eb47e7fe9
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-07-14 13:21:31 +0200

    Rename test

--- src/test/java/com/productdock/library/inventory/adapter/out/kafka/KafkaProducerShould.java
+++ src/test/java/com/productdock/library/inventory/adapter/out/kafka/KafkaProducerShould.java
@@ -18,19 +18,11 @@ class KafkaProducerShould {
     private final String topic = "test-book-availability";
 
     @Test
-    void produceCorrectMessage() throws JsonProcessingException {
+    void produceMessage() throws JsonProcessingException {
         var bookAvailabilityMessage = new BookAvailabilityMessage("1", 1);
         var producerRecord = recordProducer.createKafkaRecord(topic, bookAvailabilityMessage);
         String desiredValue = "{\"bookId\":\"" + bookAvailabilityMessage.getBookId() + "\",\"availableBookCount\":" + bookAvailabilityMessage.getAvailableBookCount() + "}";
 
         assertThat(producerRecord.value()).isEqualTo(desiredValue);
-    }
-
-    @Test
-    void throwException_whenProducingBadEntity() {
-        BookAvailabilityMessage bookAvailabilityMessage = mock(BookAvailabilityMessage.class);
-        assertThrows(JsonProcessingException.class, () -> {
-            var producerRecord = recordProducer.createKafkaRecord(topic, bookAvailabilityMessage);
-        });
     }
 }
