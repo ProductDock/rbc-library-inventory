e1847c1451a055e6f47d6bb65c4786464401c509
Milica Zivkov <milica.zivkov@productdock.com>
2022-07-15 11:59:52 +0200

    deserializer can be moved to KafkaConsumer

--- src/main/java/com/productdock/library/inventory/adapter/in/kafka/KafkaConsumer.java
+++ src/main/java/com/productdock/library/inventory/adapter/in/kafka/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.productdock.library.inventory.adapter.in.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.productdock.library.inventory.adapter.in.kafka.messages.BookRentalStatusChanged;
 import com.productdock.library.inventory.adapter.in.kafka.messages.BookRentalsMapper;
 import com.productdock.library.inventory.application.port.in.UpdateBookStatusUseCase;
 import lombok.extern.slf4j.Slf4j;
@@ -11,14 +13,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public record KafkaConsumer(UpdateBookStatusUseCase updateBookStatusUseCase,
-                            RentalRecordDeserializer rentalRecordDeserializer, BookRentalsMapper bookRentalsMapper) {
+                            BookRentalsMapper bookRentalsMapper,
+                            ObjectMapper objectMapper) {
 
     @KafkaListener(topics = "${spring.kafka.topic.book-status}")
     public synchronized void listen(ConsumerRecord<String, String> message) throws JsonProcessingException {
         log.debug("Received kafka message: {}", message);
 
-        var rentalRecordMessage = rentalRecordDeserializer.deserializeRentalRecord(message);
+        var rentalRecordMessage = deserializeMessageFromJson(message.value());
         var rentalRecord = bookRentalsMapper.toDomain(rentalRecordMessage);
         updateBookStatusUseCase.updateBookStatus(rentalRecord);
+    }
+
+    private BookRentalStatusChanged deserializeMessageFromJson(String message) throws JsonProcessingException {
+        return objectMapper.readValue(message, BookRentalStatusChanged.class);
     }
 }
