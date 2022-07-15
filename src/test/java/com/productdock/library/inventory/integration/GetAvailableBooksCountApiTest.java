65b7a4e2b73b13600ab015719432501d89a0686b
Milica Zivkov <milica.zivkov@productdock.com>
2022-07-15 16:00:53 +0200

    name integration test after use case

--- src/test/java/com/productdock/library/inventory/integration/GetAvailableBooksCountApiTest.java
+++ src/test/java/com/productdock/library/inventory/integration/GetAvailableBooksCountApiTest.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class InventoryRecordApiTest extends KafkaTestBase {
+class GetAvailableBooksCountApiTest extends KafkaTestBase {
 
     public static final String BOOK_ID = "1";
 
