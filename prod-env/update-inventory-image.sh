ed9255e81e8041b291223b8b9b84aef1d1009af8
dusanpetrovicpd <100684501+dusanpetrovicpd@users.noreply.github.com>
2022-08-22 13:40:16 +0200

    Authenticate with our custom jwt token

--- prod-env/update-inventory-image.sh
+++ prod-env/update-inventory-image.sh
@@ -6,5 +6,5 @@ docker stop rbc-library-inventory
 docker rm rbc-library-inventory
 docker rmi $(docker images | grep "rbc-library-inventory")
 
-docker run -dp 8084:8080 -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL --env-file /home/pd-library/.inventory-service_env --name=rbc-library-inventory gcr.io/prod-pd-library/rbc-library-inventory:$1
+docker run -dp 8084:8080 -v $INVENTORY_JWT_PUBLIC_KEY:/app/app.pub:ro -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL --env-file /home/pd-library/.inventory-service_env --name=rbc-library-inventory gcr.io/prod-pd-library/rbc-library-inventory:$1
 docker container ls -a
