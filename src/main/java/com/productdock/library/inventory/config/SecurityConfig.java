b49c2341e253edb7f60785a1beb6047a2fd3078a
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-12-22 13:24:54 +0100

    Optimize imports

--- src/main/java/com/productdock/library/inventory/config/SecurityConfig.java
+++ src/main/java/com/productdock/library/inventory/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.productdock.library.inventory.config;
 
 import org.springframework.beans.factory.annotation.Value;
-
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
