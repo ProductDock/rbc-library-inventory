934743eade9f3d9d5d2bb968c1e4d0ce3be00fb8
nenadbecanovicpd <nenad.becanovic@productdock.com>
2023-03-29 10:44:46 +0200

    Insert new book from kafka topic

--- src/main/java/com/productdock/library/inventory/adapter/in/kafka/KafkaConsumer.java
+++ src/main/java/com/productdock/library/inventory/adapter/in/kafka/KafkaConsumer.java
@@ -2,8 +2,11 @@ package com.productdock.library.inventory.adapter.in.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.productdock.library.inventory.adapter.in.kafka.messages.InsertBookMessage;
 import com.productdock.library.inventory.adapter.in.kafka.messages.BookRentalStatusChanged;
 import com.productdock.library.inventory.adapter.in.kafka.messages.BookRentalsMapper;
+import com.productdock.library.inventory.adapter.in.kafka.messages.InventoryMapper;
+import com.productdock.library.inventory.application.port.in.InsertBookUseCase;
 import com.productdock.library.inventory.application.port.in.UpdateBookStockUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -13,19 +16,34 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public record KafkaConsumer(UpdateBookStockUseCase updateBookStockUseCase,
+                            InsertBookUseCase insertBookUseCase,
                             BookRentalsMapper bookRentalsMapper,
+                            InventoryMapper inventoryMapper,
                             ObjectMapper objectMapper) {
 
     @KafkaListener(topics = "${spring.kafka.topic.book-status}")
-    public synchronized void listen(ConsumerRecord<String, String> message) throws JsonProcessingException {
-        log.debug("Received kafka message: {}", message);
+    public synchronized void listenBookStatus(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        log.debug("Received book status kafka message: {}", message);
 
-        var bookRentalStatusChanged = deserializeMessageFromJson(message.value());
+        var bookRentalStatusChanged = deserializeBookRentalStatusChangedMessageFromJson(message.value());
         var bookRentals = bookRentalsMapper.toDomain(bookRentalStatusChanged);
         updateBookStockUseCase.updateBookStock(bookRentals);
     }
 
-    private BookRentalStatusChanged deserializeMessageFromJson(String message) throws JsonProcessingException {
+    @KafkaListener(topics = "${spring.kafka.topic.insert-inventory}")
+    public synchronized void listenInsertInventory(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        log.debug("Received insert inventory kafka message: {}", message);
+
+        var insertBookMessage = deserializeInsertBookMessageFromJson(message.value());
+        var newInventory = inventoryMapper.toDomain(insertBookMessage);
+        insertBookUseCase.insertBook(newInventory);
+    }
+
+    private BookRentalStatusChanged deserializeBookRentalStatusChangedMessageFromJson(String message) throws JsonProcessingException {
         return objectMapper.readValue(message, BookRentalStatusChanged.class);
+    }
+
+    private InsertBookMessage deserializeInsertBookMessageFromJson(String message) throws JsonProcessingException {
+        return objectMapper.readValue(message, InsertBookMessage.class);
     }
 }
