dbe1a99be540ca9448f0131b8ded0ee53c13e07c
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-03-22 09:25:18 +0100

    Fix delete book service test

--- src/test/java/com/productdock/library/inventory/application/service/DeleteBookServiceShould.java
+++ src/test/java/com/productdock/library/inventory/application/service/DeleteBookServiceShould.java
@@ -2,7 +2,6 @@ package com.productdock.library.inventory.application.service;
 
 import com.productdock.library.inventory.application.port.out.persistence.InventoryRecordsPersistenceOutPort;
 import com.productdock.library.inventory.domain.Inventory;
-import com.productdock.library.inventory.domain.exception.InventoryException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,32 +11,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteBookServiceShould {
 
-    private static  final String BOOK_ID = "1";
+    private static final String BOOK_ID = "1";
     @InjectMocks
     private DeleteBookService deleteBookService;
     @Mock
     private InventoryRecordsPersistenceOutPort inventoryRecordRepository;
 
     @Test
-    void deleteBookInventory(){
+    void deleteBookInventory() {
         var inventory = mock(Inventory.class);
         given(inventoryRecordRepository.findByBookId(BOOK_ID)).willReturn(Optional.of(inventory));
         deleteBookService.deleteBook(BOOK_ID);
 
         verify(inventoryRecordRepository).deleteByBookId(BOOK_ID);
     }
-
-    @Test
-    void deleteBookInventory_WhenInventoryDoesntExist(){
-        given(inventoryRecordRepository.findByBookId(BOOK_ID)).willReturn(Optional.empty());
-
-        assertThrows(InventoryException.class, () -> deleteBookService.deleteBook(BOOK_ID));
-    }
-
 }
