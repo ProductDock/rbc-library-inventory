2046af143de08cd8092fd2f8777f20f05cb5ccef
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-04-01 11:23:29 +0200

    Add tests when book subscribers list is null

--- src/test/java/com/productdock/library/inventory/domain/BookSubscriptionsShould.java
+++ src/test/java/com/productdock/library/inventory/domain/BookSubscriptionsShould.java
@@ -19,6 +19,15 @@ class BookSubscriptionsShould {
     }
 
     @Test
+    void subscribeUserWhenListIsNull() {
+        var subscriptions = new BookSubscriptions();
+
+        subscriptions.subscribeUser(USER_ID);
+
+        assertThat(subscriptions.getSubscriberUserIds()).hasSize(1);
+    }
+
+    @Test
     void unsubscribeUser() {
         var subscriptions = bookSubscriptions();
         subscriptions.subscribeUser(USER_ID);
@@ -29,11 +38,27 @@ class BookSubscriptionsShould {
     }
 
     @Test
+    void unsubscribeUserWhenListIsNull() {
+        var subscriptions = new BookSubscriptions();
+
+        subscriptions.unsubscribeUser(USER_ID);
+
+        assertThat(subscriptions.getSubscriberUserIds()).isNull();
+    }
+
+    @Test
     void checkSubscription() {
         var subscription = bookSubscriptions();
         subscription.subscribeUser(USER_ID);
 
         assertThat(subscription.isUserSubscribed(USER_ID)).isTrue();
+    }
+
+    @Test
+    void checkSubscriptionWhenListIsNull() {
+        var subscription = new BookSubscriptions();
+
+        assertThat(subscription.isUserSubscribed(USER_ID)).isFalse();
     }
 
 }
