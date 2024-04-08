ae7ab2a8e909d8c107271cd4ee3818595430a972
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-04-08 09:07:26 +0200

    Add duplicate subscription save handling

--- src/main/java/com/productdock/library/inventory/adapter/out/mongo/BookSubscriptionPersistenceAdapter.java
+++ src/main/java/com/productdock/library/inventory/adapter/out/mongo/BookSubscriptionPersistenceAdapter.java
@@ -1,5 +1,6 @@
 package com.productdock.library.inventory.adapter.out.mongo;
 
+import com.mongodb.MongoWriteException;
 import com.productdock.library.inventory.adapter.out.mongo.mapper.BookSubscriptionsMapper;
 import com.productdock.library.inventory.application.port.out.persistence.BookSubscriptionPersistenceOutPort;
 import com.productdock.library.inventory.domain.BookSubscription;
@@ -19,7 +20,15 @@ public class BookSubscriptionPersistenceAdapter implements BookSubscriptionPersi
 
     @Override
     public void save(BookSubscription subscriptions) {
-        subscriptionsRepository.save(subscriptionsMapper.toEntity(subscriptions));
+        try {
+            subscriptionsRepository.save(subscriptionsMapper.toEntity(subscriptions));
+        } catch (Exception e) {
+            if (e.getCause() instanceof MongoWriteException) {
+                log.warn("Ignoring duplicate key error: " + e.getMessage());
+            } else {
+                throw e;
+            }
+        }
     }
 
     @Override
