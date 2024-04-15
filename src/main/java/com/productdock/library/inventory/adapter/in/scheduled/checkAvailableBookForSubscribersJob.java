0b9bdeb1294562628c6db13ad1c7b206260e32d4
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-04-15 14:05:35 +0200

    Add job for checking subscriptions

---
+++ src/main/java/com/productdock/library/inventory/adapter/in/scheduled/checkAvailableBookForSubscribersJob.java
@@ -0,0 +1,22 @@
+package com.productdock.library.inventory.adapter.in.scheduled;
+
+import com.productdock.library.inventory.application.port.in.CheckAvailableBooksForSubscriptionsUseCase;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class checkAvailableBookForSubscribersJob {
+
+    private CheckAvailableBooksForSubscriptionsUseCase checkAvailableBooksForSubscriptionsUseCase;
+
+    @Scheduled(cron = "${availability.auto-check.scheduled}")
+    public void checkAvailablebooks() {
+        log.warn("Started scheduled task for checking book availability");
+        checkAvailableBooksForSubscriptionsUseCase.checkAvailableBooks();
+    }
+
+}
